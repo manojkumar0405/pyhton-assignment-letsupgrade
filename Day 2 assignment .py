fe8ed80ea28cb7c88088d3f9763ec4dba2f56dc3
manojkumar0405 <68554090+manojkumar0405@users.noreply.github.com>
2020-07-21 21:03:52 +0530

    Add files via upload

---
+++ Day 2 assignment .py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+print("coder's python")
+
+
+# In[7]:
+
+
+print('let\'s upgrade')
+
+
+# In[14]:
+
+
+print("python\nlanguage")
+
+
+# In[15]:
+
+
+print("why\tpython")
+
+
+# In[16]:
+
+
+print('why\asequence')
+
+
+# In[35]:
+
+
+object="school"
+revenue = 87.987
+old = 25
+print("studied in",object,"fees",revenue,"est in",old)
+
+
+# In[33]:
+
+
+print ("studied in %s fees %f est in %d"%(object,revenue,old))
+
+
+# In[39]:
+
+
+print ("studied in %5s fees %3.3f est in %2d"%(object,revenue,old))
+
+
+# In[45]:
+
+
+print (f"studied in {object},fees is {revenue} ,est in {old}.")
+
+
+# In[47]:
+
+
+bin(246)
+
+
+# In[52]:
+
+
+x=10;y=120
+y
+
+
+# In[53]:
+
+
+id(y)
+
+
+# In[54]:
+
+
+id(x)
+
+
+# In[92]:
+
+
+x=10;y=10
+x is y
+
+
+# In[65]:
+
+
+7**5
+
+
+# In[67]:
+
+
+10/2,10%3
+
+
+# In[73]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[75]:
+
+
+a=4;b=3
+a==b
+
+
+# In[76]:
+
+
+a>b;b<a
+
+
+# In[81]:
+
+
+a<b & b>k
+
+
+# In[88]:
+
+
+str="knowledge's gain"
+"z" in str
+"y" not in str
+
+
+# In[93]:
+
+
+x=y=259
+x is y
+
+
+# In[95]:
+
+
+a!=b
+
+
+# In[ ]:
+
+
+
+
