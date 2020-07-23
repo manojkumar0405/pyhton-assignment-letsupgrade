98f0c110243fc773f03194158b521b3eac9bcb4d
manojkumar0405 <68554090+manojkumar0405@users.noreply.github.com>
2020-07-23 19:58:29 +0530

    Add files via upload

---
+++ Day-4 assignment.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+str1=("learning python on let'supgrade,easy to  gain knowledgein this pandemic situation,thanks letsupgrade")
+str1.count("")
+
+    
+
+
+# In[23]:
+
+
+str1=("learning python on let'supgrade,easy to  gain knowledgein this pandemic situation,thanks letsupgrade")
+len(str1)
+
+
+# In[29]:
+
+
+str1=("learning python on let'supgrade,easy to  gain knowledgein this pandemic situation,thanks to To letsupgrade")
+str1.count("to")
+
+
+# In[38]:
+
+
+a=input ("enter the value")
+if a.islower():
+    a=var(a)
+print(a,type(a))    
+
+
+# In[36]:
+
+
+"DEWDED".islower()
+
+
+# In[37]:
+
+
+"derrfr".islower()
+
+
+# In[40]:
+
+
+"FFRRR".isupper()
+
+
+#    
+
+# In[46]:
+
+
+num=input("value")
+if num.isdigit():
+    num=int(num)
+print(num,type(num)) 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
