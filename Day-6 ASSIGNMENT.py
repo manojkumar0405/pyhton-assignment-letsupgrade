447b7ab6b8120930f24bc7644a7585e315b649d4
manojkumar0405 <68554090+manojkumar0405@users.noreply.github.com>
2020-07-28 11:00:41 +0530

    Add files via upload

---
+++ Day-6 ASSIGNMENT.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+alist = [1,2,3,4,5,6,7,8]
+blist = ["a","b","c","d","e"]
+print (list(zip(alist,blist)))
+
+
+# In[25]:
+
+
+
+alist=[0,1,2,10,4,1,0,56,2,0,1,3,0,56,0,4]
+alist.sort(key=,reverse=56) 
+print(alist)
+
+
+# In[10]:
+
+
+alist
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
